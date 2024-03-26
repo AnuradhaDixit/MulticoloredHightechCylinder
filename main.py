a2023e6f4c428a332b3aebffcca8a1612b370da4
Anuradha Dixit <82029747+AnuradhaDixit@users.noreply.github.com>
2024-03-26 12:22:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-number =  int(input('Enter a number:'))
-if number % 2 == 0:
-  print('Even')
-else:
-  print('Odd')
+height = float(input("enter your height in m: "))
+weight = int(input("enter your weight in kg: "))
+BMI = weight / (height ** 2)
+if BMI <= 18.5:
+  print('you are underweight')
+elif BMI <= 25:
+  print('you are normal weight')
+elif BMI >= 25:
+  print('you are overweight')
+else :
+  print('you are obese'+f'your BMI is {BMI}')
